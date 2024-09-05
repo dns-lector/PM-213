46551116bcf2bfd2b19ea47e7c5c6e30b2e43003
dns-lector <157026463+dns-lector@users.noreply.github.com>
2024-09-05 13:19:04 +0300

    Saves

--- App/RomanNumber.cs
+++ App/RomanNumber.cs
@@ -8,16 +8,34 @@ namespace App
 {
     public record RomanNumber(int Value)
     {
-        private readonly int _value = Value;
+        private readonly int _value = Value;  // TODO: Refactoring - exclude
         public int Value => _value;
 
         public static RomanNumber Parse(String input)
         {
             int value = 0;
-            int prevDigit = 0;
+            int prevDigit = 0;   // TODO: rename to ~rightDigit
+            int pos = input.Length;
             foreach (char c in input.Reverse())
             {
-                int digit = DigitValue(c.ToString());
+                pos -= 1;
+                int digit;
+                try
+                {
+                    digit = DigitValue(c.ToString());
+                }
+                catch
+                {
+                    throw new FormatException(
+                        $"Invalid symbol '{c}' in position {pos}");
+                }
+
+                if(digit != 0 && prevDigit / digit > 10)
+                {
+                    throw new FormatException(
+                        $"Invalid order '{c}' before '{input[pos + 1]}' in position {pos}");
+                }
+
                 if (digit >= prevDigit)
                 {
                     value += digit;
@@ -42,8 +60,51 @@ namespace App
             "D" => 500,
             "M" => 1000,
             _ => throw new ArgumentException($"{nameof(RomanNumber)}::{nameof(DigitValue)}: 'digit' has invalid value '{digit}'")
-            // _ => throw new ArgumentException("Invalid Roman digit.")
         };
+
+        public override string? ToString()
+        {
+            // 3343 -> MMMCCCXLIII
+            // M M M
+            // D (500) x
+            // CD (400) x
+            // C C C
+            // L x
+            // XL 
+            // X x
+            // V x
+            // IV
+            // III
+            if (_value == 0) return "N";
+            Dictionary<int, String> parts = new()
+            {
+                { 1000, "M" },
+                { 900, "CM" },
+                { 500, "D" },
+                { 400, "CD" },
+                { 100, "C" },
+                { 90, "XC" },
+                { 50, "L" },
+                { 40, "XL" },
+                { 10, "X" },
+                { 9, "IX" },
+                { 5, "V" },
+                { 4, "IV" },
+                { 1, "I" },
+            };
+            int v = _value;
+            StringBuilder sb = new();
+            foreach (var part in parts)
+            {
+                while(v >= part.Key)
+                {
+                    v -= part.Key;
+                    sb.Append(part.Value);
+                }
+            }
+            return sb.ToString();
+        }
+
     }
 
 }
--- Test/RomanNumberTest.cs
+++ Test/RomanNumberTest.cs
@@ -5,6 +5,18 @@ namespace Test
     [TestClass]
     public class RomanNumberTest
     {
+        private readonly Dictionary<String, int> _digitValues = new()
+        {
+            { "N", 0    },
+            { "I", 1    },
+            { "V", 5    },
+            { "X", 10   },
+            { "L", 50   },
+            { "C", 100  },
+            { "D", 500  },
+            { "M", 1000 },
+        };
+
         [TestMethod]
         public void ParseTest()
         {
@@ -37,23 +49,56 @@ namespace Test
                     $"{testCase.Key} -> {testCase.Value}"
                 );
             }
+            Dictionary<String, Object[]> exTestCases = new()
+            {
+                { "W", ['W', 0] },
+                { "Q", ['Q', 0] },
+                { "s", ['s', 0] },
+                { "sX", ['s', 0] },
+                { "Xd", ['d', 1] },
+            };
+            foreach (var testCase in exTestCases)
+            {
+                var ex = Assert.ThrowsException<FormatException>(
+                    () => RomanNumber.Parse(testCase.Key),
+                    $"Parse '{testCase.Key}' must throw FormatException"
+                );
+                Assert.IsTrue(
+                    ex.Message.Contains(
+                        $"Invalid symbol '{testCase.Value[0]}' in position {testCase.Value[1]}"
+                    ),
+                    "FormatException must contain data about symbol and its position"
+                    + $"testCase: '{testCase.Key}', ex.Message: '{ex.Message}'"
+                );
+            }
+            Dictionary<String, Object[]> exTestCases2 = new()
+            {
+                { "IM",  ['I', 'M', 0] },
+                { "XIM", ['I', 'M', 1] },
+                { "IMX", ['I', 'M', 0] },
+                { "XMD", ['X', 'M', 0] },
+                { "XID", ['I', 'D', 1] },
+            };
+            foreach (var testCase in exTestCases2)
+            {
+                var ex = Assert.ThrowsException<FormatException>(
+                    () => RomanNumber.Parse(testCase.Key),
+                    $"Parse '{testCase.Key}' must throw FormatException"
+                );
+                Assert.IsTrue(
+                    ex.Message.Contains(
+                        $"Invalid order '{testCase.Value[0]}' before '{testCase.Value[1]}' in position {testCase.Value[2]}"
+                    ),
+                    "FormatException must contain data about mis-ordered symbols and its position"
+                    + $"testCase: '{testCase.Key}', ex.Message: '{ex.Message}'"
+                );
+            }
         }
 
         [TestMethod]
         public void DigitValueTest()
-        {
-            Dictionary<String, int> testCases = new()
-            {
-                { "N", 0    },
-                { "I", 1    },
-                { "V", 5    },
-                { "X", 10   },
-                { "L", 50   },
-                { "C", 100  },
-                { "D", 500  },
-                { "M", 1000 },
-            };
-            foreach (var testCase in testCases)
+        {           
+            foreach (var testCase in _digitValues)
             {
                 Assert.AreEqual(
                     testCase.Value, 
@@ -65,7 +110,11 @@ namespace Test
             for (int i = 0; i < 100; ++i)
             {
                 String invalidDigit = ((char) random.Next(256)).ToString();
-
+                if(_digitValues.ContainsKey(invalidDigit))
+                {
+                    --i;
+                    continue;
+                }
                 ArgumentException ex =
                     Assert.ThrowsException<ArgumentException>(
                         () => RomanNumber.DigitValue(invalidDigit),
@@ -92,12 +141,41 @@ namespace Test
                 );
             }
         }
+
+        [TestMethod]
+        public void ToStringTest()
+        {
+            Dictionary<int, String> testCases = new() {   // Append / Concat
+                { 2, "II" },
+                { 3343, "MMMCCCXLIII" },
+                { 4, "IV" },
+                { 44, "XLIV" },
+                { 9, "IX" },
+                { 90, "XC" },
+                { 1400, "MCD" },
+                { 999, "CMXCIX" },   // непрямо - заборона IM
+                { 444, "CDXLIV" },
+                { 990, "CMXC" },   // непрямо - заборона XM
+            };
+
+            _digitValues.Keys.ToList().ForEach(k => testCases.Add(_digitValues[k], k));
+            
+            foreach (var testCase in testCases)
+            {
+                Assert.AreEqual(
+                    testCase.Value,
+                    new RomanNumber(testCase.Key).ToString(),
+                    $"ToString({testCase.Key}) --> {testCase.Value}"
+                );
+            }
+        }
     }
 }
-/* Д.З. Збільшити кількість тестових кейсів для ParseTest
- * Використовувати як оптимальні, так і неоптимальні форми чисел.
- * Перевірити працездатність шляхом включення неправильних кейсів
- * 
+/* Д.З. Збільшити кількість тестових кейсів виняткових ситуацій ParseTest
+ * які відстежують неправильне розташування символів (IM, ID, XM, ...)
+ * у різних позиціях
+ * * додати кейси з іншими неправильними комбінаціями (VV, LL, LC, VX, ...)
+ *    внести зміни в алгоритм парсингу
  * 
  * 
  * 
